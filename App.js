d90b7bccaafa43700c37491e87d0fb1269f162a8
Shashank J <shashankreddy231@gmail.com>
2019-04-22 22:04:29 +0530

    Initial Commit.
    Initial Commit with auth screen.

--- App.js
+++ App.js
@@ -1,39 +1,64 @@
-import React, { Component } from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView, Dimensions } from 'react-native';
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
 
 import firebase from 'react-native-firebase';
-import FadingSlides from 'react-native-fading-slides'
 
-const { width, height } = Dimensions.get('window');
-
-
-const slides = [
-  {
-    image: require('./assets/sapphire.png'),
-    imageWidth: 250,
-    imageHeight: 250,
-    title: 'Sapphire Coders',
-    subtitle: 'SSCW - SVIT',
-    titleColor: '#fff',
-    subtitleColor: '#fff',
-  },
-  {
-    image: require('./assets/sapphire1.png'),
-    imageWidth: 250,
-    imageHeight: 250,
-    title: 'Shashank J',
-    subtitle: 'sscw',
-    titleColor: '#fff',
-    subtitleColor: '#fff',
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
   }
-]
 
-export default class App extends Component {
-  render () {
+  async componentDidMount() {
+    // TODO: You: Do firebase things
+    // const { user } = await firebase.auth().signInAnonymously();
+    // console.warn('User -> ', user.toJSON());
+
+    // await firebase.analytics().logEvent('foo', { bar: '123'});
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        <FadingSlides slides={slides} fadeDuration={900} stillDuration={1700} height={height} />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
+          <Text style={styles.welcome}>
+            Welcome to {'\n'} React Native Firebase
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit App.js
+          </Text>
+          {Platform.OS === 'ios' ? (
+            <Text style={styles.instructions}>
+              Press Cmd+R to reload,{'\n'}
+              Cmd+D or shake for dev menu
+            </Text>
+          ) : (
+            <Text style={styles.instructions}>
+              Double tap R on your keyboard to reload,{'\n'}
+              Cmd+M or shake for dev menu
+            </Text>
+          )}
+          <View style={styles.modules}>
+            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
+            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
+            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
+            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
+            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
+            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
+            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
+            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
+            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
+            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
+            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
+            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
+            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
+            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
+            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
+            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -43,6 +68,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000',
+    backgroundColor: '#F5FCFF',
   },
+  logo: {
+    height: 120,
+    marginBottom: 16,
+    marginTop: 64,
+    padding: 10,
+    width: 135,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  modules: {
+    margin: 20,
+  },
+  modulesHeader: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  module: {
+    fontSize: 14,
+    marginTop: 4,
+    textAlign: 'center',
+  }
 });
