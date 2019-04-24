61706ef2527fe2056f780f2855aeac032c774d08
Shashank J <shashankreddy231@gmail.com>
2019-04-24 18:55:04 +0530

    Final Drawer Navigation.

--- src/authscreens/Main.js
+++ src/authscreens/Main.js
@@ -1,30 +1,96 @@
-import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
-import firebase from 'react-native-firebase'
-import BottomTabNavigator from '../MainNavigators/BottomTabNavigator'
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  Platform,
+  View,
+  StatusBar,
+  DrawerLayoutAndroid,
+} from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from '../Redux/reducers';
+import { setNavigator, setActiveRoute } from "../Redux/actions"
+import Toolbar from '../Navigation/Toolbar';
+import AppNavigation from '../Navigation/AppNavigation';
+import { bgStatusBar, bgDrawer } from '../global.styles';
+import DrawerContent from '../Navigation/DrawerContent'
 
-export default class Main extends React.Component {
-  state = { currentUser: null }
 
-  componentDidMount() {
-    const { currentUser } = firebase.auth()
+let store = createStore(reducer);
+/* getDrawerWidth       Default drawer width is screen width - header width
+* https://material.io/guidelines/patterns/navigation-drawer.html
+*/
+const getDrawerWidth = () => Dimensions.get('window').width - (Platform.OS === 'android' ? 56 : 64);
 
-    this.setState({ currentUser })
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.drawer = React.createRef();
+    this.navigator = React.createRef();
   }
 
-  render() {
-    const { currentUser } = this.state
+  componentDidMount() {
+    store.dispatch(setNavigator(this.navigator.current));
+  }
 
+  openDrawer = () => {
+    this.drawer.current.openDrawer();
+  };
+
+  closeDrawer = () => {
+    this.drawer.current.closeDrawer();
+  };
+
+  getActiveRouteName = navigationState => {
+    if (!navigationState) {
+      return null;
+    }
+    const route = navigationState.routes[navigationState.index];
+    // dive into nested navigators
+    if (route.routes) {
+      return getActiveRouteName(route);
+    }
+    return route.routeName;
+  };
+
+  render() {
     return (
-      <BottomTabNavigator />
-    )
+      <Provider store={store}>
+        <DrawerLayoutAndroid
+          drawerWidth={getDrawerWidth()}
+          drawerPosition={DrawerLayoutAndroid.positions.Left}
+          renderNavigationView={
+            () => <DrawerContent closeDrawer={this.closeDrawer} />
+          }
+          drawerBackgroundColor={bgDrawer}
+          ref={this.drawer}
+        >
+          <View style={styles.container}>
+            <StatusBar
+                translucent
+                backgroundColor={bgStatusBar}
+                animated
+            />
+            <Toolbar showMenu={this.openDrawer} />
+            <AppNavigation
+              onNavigationStateChange={(prevState, currentState) => {
+                const currentScreen = this.getActiveRouteName(currentState);
+                store.dispatch(setActiveRoute(currentScreen));
+              }}
+              ref={this.navigator}
+            />
+          </View>
+        </DrawerLayoutAndroid>
+      </Provider>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    backgroundColor: '#fff'
+  },
+});
