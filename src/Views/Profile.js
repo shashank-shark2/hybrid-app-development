5bcededc7c490c8f759fa3fde94ecab5096fa7e2
Shashank J <shashankreddy231@gmail.com>
2019-04-25 20:19:27 +0530

    Added profile pic

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+// import ProfileComponent from '../screen-components/ProfileComponent/ProfileComponent'
+import ProfileComp from '../screen-components/ProfileComponent/screens/Profile1/index'
 
 export default class Profile extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.TextHere}>Profile Screen</Text>
-      </View>
+      <ProfileComp />
     )
   }
 }
