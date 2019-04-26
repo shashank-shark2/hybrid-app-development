8147993b595286db91352b68181baa70dd90dfaf
Shashank J <shashankreddy231@gmail.com>
2019-04-26 19:14:04 +0530

    import made successfully

---
+++ src/screen-components/HomeScreenComponents/Personal/Screen4.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+export default class Screen4 extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.TextHere}>Screen 1</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    TextHere: {
+        alignContent: 'center',
+        justifyContent: 'center',
+    }
+})
