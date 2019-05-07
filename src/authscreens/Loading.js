70ac82ec49c65407bb2470d0e8a0ed2cdbbfe525
Shashank J <shashankreddy231@gmail.com>
2019-05-07 19:17:14 +0530

    fixed bug for splash screen stuck at beginning

--- src/authscreens/Loading.js
+++ src/authscreens/Loading.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
 import firebase from 'react-native-firebase'
+import SplashScreen from 'react-native-splash-screen'
 
 export default class Loading extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       this.props.navigation.navigate(user ? 'Main' : 'SignUp')
     })
+    SplashScreen.hide();
   }
 
   render() {
